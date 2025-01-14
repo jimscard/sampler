b965914ff60d62eac2c1f50aec5f0e59e03e9694
Jim Scardelis <jim@jceltd.com>
2025-01-14 08:04:30 +0000

    Change to ceil instead of floor to round percentage up.

--- sampler.py
+++ sampler.py
@@ -2,10 +2,11 @@
 import csv
 import sys
 import os
-from hashlib import sha256 
+from hashlib import sha256
 import random
 import math
 import secrets
+
 
 def main():
     """
@@ -27,13 +28,13 @@ def main():
     filepath = os.getcwd() + '/' + filename
 
     # Ensure the file path is within the current working directory
-    if not os.path.abspath(os.path.realpath(filepath)).startswith(os.getcwd()): # import os
+    if not os.path.abspath(os.path.realpath(filepath)).startswith(os.getcwd()):  # import os
         raise RuntimeError('Filepath falls outside the base directory')
-    
+
     # Open the CSV file for reading, ignoring any errors
-    with open(filepath, 'r', errors='ignore') as csv_in: 
+    with open(filepath, 'r', errors='ignore') as csv_in:
         # Define the output file name
-        csv_out = "samples-" + filename 
+        csv_out = "samples-" + filename
         # Read all lines from the input file
         lines = csv_in.readlines()  # Read the file into a list of lines
         # Seed the random number generator for sampling
@@ -41,14 +42,18 @@ def main():
         # Determine the total number of lines
         population = len(lines)
         # Calculate the sample size (25% of total lines or 25, whichever is smaller)
-        sample_size = min(math.floor(population * 0.25), 25)
+        sample_size = min(math.ceil(population * 0.25), 25)
         # Randomly sample the lines
-        sample_lines = random.sample(lines, sample_size)  
-        
+        sample_lines = random.sample(lines, sample_size)
+
         # Write the sampled lines to the output file
-        with open(csv_out, 'w') as f: 
-            f.write(''.join(sample_lines))  # Write the sampled lines to a new file
+        with open(csv_out, 'w') as f:
+            # Write the sampled lines to a new file
+            f.write(''.join(sample_lines))
         # Print the result of the sampling process
-        print(f"{sample_size} samples out of a population of {population} written to {csv_out}")
-if __name__ == "__main__": 
+        print(f"{sample_size} samples out of a population of {
+              population} written to {csv_out}")
+
+
+if __name__ == "__main__":
     main()
